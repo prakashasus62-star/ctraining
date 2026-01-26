7853b45381a5156216b1defa91df6500b1ee9aed
prakashasus62-star <prakashasus62@gamil.com>
2026-01-26 19:48:24 +0530

    Bill problem

---
+++ if else Statement/bill.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int units=120;
+    float billamount;
+    if(units<=100){
+        billamount=units*5;
+    }
+    else{
+        billamount=units*7;
+    }
+    return 0;
+    }
