626e365e627360628037800be3b8386e3be36e5b
prakashasus62-star <prakashasus62@gamil.com>
2026-01-26 13:51:30 +0530

    3 digit

---
+++ if else Statement/3digit.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int n=345;
+    if(n>=100 && n<=999){
+        printf("Three digit number");
+    }
+    else{
+        printf("Not a three digirt number");
+    }
+
+}
