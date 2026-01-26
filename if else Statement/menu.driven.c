2134e396d0222b9896bd2e1dbf7b60ad6228ee5b
prakashasus62-star <prakashasus62@gamil.com>
2026-01-26 21:42:52 +0530

    menu driven

---
+++ if else Statement/menu.driven.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    int k=1;
+    switch(k){
+        case 1:
+        int a=10;
+        int b=20;
+        printf("%d",a+b);
+        break;
+        default:
+        printf("invalid");
+    }
+    return 0;
+    }
