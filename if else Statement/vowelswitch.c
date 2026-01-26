00d371057533f0c64481516e18a8ec392a4e2697
prakashasus62-star <prakashasus62@gamil.com>
2026-01-26 20:15:13 +0530

    Day switch

---
+++ if else Statement/vowelswitch.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(){
+    int k=3;
+    switch(k){
+        case 1:
+        printf("monday");
+        break;
+        case 2:
+        printf("tuesday");
+        break;
+        case 3:
+        printf("wednesday");
+        break;
+        default:
+        printf("invalid");
+    }
+    return 0;
+    }
