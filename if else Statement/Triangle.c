49fbc0c1cd105e05dbc669b39ae8b7417cab2f4b
prakashasus62-star <prakashasus62@gamil.com>
2026-01-26 14:06:05 +0530

    Valid Triangle

---
+++ if else Statement/Triangle.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int n1=60;
+    int n2=60;
+    int n3=60;
+    int sum=n1+n2+n3;
+
+    if(sum==180){
+        printf("Valid Triangle");
+    }
+    return 0;
+    }
