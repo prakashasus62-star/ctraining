889012e1ec2c63417002adfe35043b57c6b78136
prakashasus62-star <prakashasus62@gamil.com>
2026-01-26 19:35:47 +0530

    Type of Triangle

---
+++ if else Statement/typeoftri.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    int n1=5;
+    int n2=5;
+    int n3=5;
+
+    if(n1==5 && n2==5 && n3==5){
+        printf("Alla sides are equal");
+    }
+    else{
+        printf("All sides are not equal");
+    }
+    return 0;
+    }
