f7ec9acc5dbe6ba24c59a849f0be5994da0813cb
prakashasus62-star <prakashasus62@gamil.com>
2026-01-26 13:27:46 +0530

    Vowel

---
+++ if else Statement/vowel.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+     char ch='a';
+     char ch2='A';
+     if(ch=='a'|| ch=='e'|| ch=='i'|| ch=='o'|| ch=='u'|| ch2=='A'|| ch2=='E'|| ch2=='I'|| ch2=='O'|| ch2=='U'){
+         printf("Vowel");
+     }
+     else{
+        printf("Consonant");
+     }
+}
