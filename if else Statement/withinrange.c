f5b05d5c6e85561a161a647e0ba88becbee9e5d9
prakashasus62-star <prakashasus62@gamil.com>
2026-01-26 19:39:26 +0530

    Range pgm

---
+++ if else Statement/withinrange.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int n1=15;
+
+    if(n1>=10 && n1<=20){
+        printf("Number within range");
+    }
+    else{
+        printf("Number not within range");
+    }
+    return 0;
+    }
