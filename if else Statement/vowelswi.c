24abbb019c3fb044c83846b1d4434b5a6d1db1eb
prakashasus62-star <prakashasus62@gamil.com>
2026-01-26 21:40:19 +0530

    vowels

---
+++ if else Statement/vowelswi.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    int a=1;
+    switch(a){
+        case 1:
+        char ch='a';
+        if(ch=='a'|| ch=='e'|| ch=='i'|| ch=='o'|| ch=='u'|| ch2=='A'|| ch2=='E'|| ch2=='I'|| ch2=='O'|| ch2=='U'){
+         printf("Vowel");
+     }
+        break;
+        default:
+        printf("invalid");
+    }
+    return 0;
+    }
