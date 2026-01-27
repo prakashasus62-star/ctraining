6b80794c7ca5e3bbfa97cbaea23a5a28c8b229f3
prakashasus62-star <prakashasus62@gamil.com>
2026-01-27 22:47:57 +0530

    toll c

---
+++ swicth.c/toll.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int vehicleType, trips;
+    int amount = 0;
+    scanf("%d", &vehicleType);
+    scanf("%d", &trips);
+    switch (vehicleType) {
+        case 1:
+            if (trips == 1)
+                amount = 100;
+            else
+                amount = 800;
+            break;
+            case 2:
+            amount = trips * 240;
+            break;
+            default:
+            return 0;
+    }
+    printf("₹%d", amount);
+    return 0;
+}
