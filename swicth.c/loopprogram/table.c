a7b01562a59fb0f6e92f1953ad34a1d4c5e9f658
prakashasus62-star <prakashasus62@gamil.com>
2026-01-28 10:09:00 +0530

    Updated C programs

---
+++ swicth.c/loopprogram/table.c
@@ -0,0 +1,14 @@
+#include <stdio.h.>
+int main(){
+    int k;
+    int mul=0;
+    scanf("%d",&k);
+    for(int i=1; i<=10; i++){
+        for(int j=1 ;j<=10; i++){
+            mul=i*k;
+        }
+        printf("%d",mul);
+        
+    }
+        
+}
