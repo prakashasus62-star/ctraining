a9cf0d1b12364f60ed76a9817cc7ecab31d9679e
prakashasus62-star <prakashasus62@gamil.com>
2026-01-28 21:55:51 +0530

    prime nuber

--- swicth.c/loopprogram/prime.c
+++ swicth.c/loopprogram/prime.c
@@ -1,5 +1,32 @@
 #include <stdio.h>
-int main(){
-    int n;
-    scanf("%d")
+
+int main()
+{
+    int n, count = 0;
+
+    printf("Enter the value of N: ");
+    scanf("%d", &n);
+
+    for (int num = 2; num <= n; num++)
+    {
+        int isPrime = 1;
+
+        for (int i = 2; i <= num / 2; i++)
+        {
+            if (num % i == 0)
+            {
+                isPrime = 0;
+                break;
+            }
+        }
+
+        if (isPrime)
+        {
+            count++;
+        }
+    }
+
+    printf("Number of prime numbers from 1 to %d is: %d", n, count);
+
+    return 0;
 }
