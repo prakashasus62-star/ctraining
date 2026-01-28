7c65f5fbe1a3cadc809bf5102f3692bc65ff257c
prakashasus62-star <prakashasus62@gamil.com>
2026-01-28 10:59:15 +0530

    reverse c programm

--- swicth.c/loopprogram/rev.c
+++ swicth.c/loopprogram/rev.c
@@ -2,11 +2,11 @@
 int main(){
     int num=122;
     int rev=0;
-    for(int i=1; num!=0;);{
+    while (num!=0)
+    {
         int rem=num%10;
         rev=rev*10+rem;
         num=num/10;
-
     }
     printf("%d",rev);
 }
