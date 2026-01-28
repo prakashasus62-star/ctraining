1faa8ee494eee328d5cee2871efed43bc387e2c9
prakashasus62-star <prakashasus62@gamil.com>
2026-01-28 09:52:10 +0530

    even

---
+++ swicth.c/loopprogram/even.c
@@ -0,0 +1,15 @@
+#include <stdio.h.>
+int main(){
+    int k;
+    scanf("%d",&k);
+    for(int i=1;i<=k;i++){
+        if(i%2==0){
+            printf("%d",i);
+            
+        }
+
+        
+    }
+    
+        
+}
