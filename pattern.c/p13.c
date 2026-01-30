00bd28071272652cd6377a931e60f048f5a61f5a
prakashasus62-star <prakashasus62@gamil.com>
2026-01-30 05:34:50 +0530

    p13

--- pattern.c/p13.c
+++ pattern.c/p13.c
@@ -1,24 +1,42 @@
 #include <stdio.h>
 
-int main(){
-    int space = 6;
+int main()
+{
+    // int space = 6;
 
-    for(int i = 1; i <= 4; i++){   
-        for(int j = 1; j <= i; j++){
+    // for(int i = 1; i <= 4; i++){
+    //     for(int j = 1; j <= i; j++){
+    //         printf("%d", j);
+    //     }
+    //     for(int k = 1; k <= space; k++){
+    //         printf(" ");
+    //     }
+    //     for(int j = i; j >= 1; j--){
+    //         printf("%d", j);
+    //     }
+
+    //     printf("\n");
+    //     space -= 2;
+    // }
+    int space = 6;
+    for (int i = 1; i <= 4; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
             printf("%d", j);
         }
-
-
-        for(int k = 1; k <= space; k++){
+        for (int k = 1; k <= space; k++)
+        {
             printf(" ");
         }
-        for(int j = i; j >= 1; j--){
+        for (int j = i; j >= 1; j--)
+        {
             printf("%d", j);
         }
-
         printf("\n");
-        space -= 2;   
+    space-=2;
     }
+    
 
     return 0;
 }
